9492f04198dee86ec3341e61885cdfb96de906b9
AvatarVI <532765732@qq.com>
2021-05-06 15:24:23 +0800

    add jerk component

--- MPC_with_RS/MPC_Module.py
+++ MPC_with_RS/MPC_Module.py
@@ -15,7 +15,7 @@ class MPC(object):
         self.Lf = param["Lf"]
         self.ref_v = param["ref_v"]
         wg = param["wg"]
-        self.ua_bound = param["ua_bound"]
+        self.uj_bound = param["uj_bound"]
         self.uo_bound = np.array(param["uo_bound"]) * np.pi / 180
         goal = param["goal"]
         goal[2] *= np.pi / 180
@@ -57,7 +57,7 @@ class MPC(object):
         m.ref_traj = pyo.Var(m.sNum)  # reference traj
 
         m.ref_heading = pyo.Var(m.sNum)  # theta constraint
-        m.ua = pyo.Var(m.uNum, bounds=(self.ua_bound[0], self.ua_bound[1]))  # control acc as var
+        m.ua = pyo.Var(m.uNum, bounds=(self.uj_bound[0], self.uj_bound[1]))  # control acc as var
         m.uo = pyo.Var(m.uNum,
                        bounds=(self.uo_bound[0], self.uo_bound[1]))  # control steering as var -0.436332, 0.436332
 
@@ -139,7 +139,7 @@ class MPC(object):
 
     def init_model_reference(self, ref_traj, wg, goal):
         N_p = np.shape(ref_traj)[1]
-        self.N_s = 8
+        self.N_s = 9
         # goal[:2] = ref_traj[:, -1]
 
         m = pyo.ConcreteModel()
@@ -150,16 +150,15 @@ class MPC(object):
         # constant Parameters
         m.wg = pyo.Param(pyo.RangeSet(0, 5), initialize={0: wg[0], 1: wg[1], 2: wg[2], 3: wg[3], 4: wg[4], 5: wg[5]},
                          mutable=True)  # weights for adjusting
-        # m.dt = pyo.Param(initialize=self.dt, mutable=True)  # integration time todo change to variables
         m.Lf = pyo.Param(initialize=self.Lf, mutable=True)  # car length between wheels axes
         m.ref_v = pyo.Param(initialize=self.ref_v, mutable=True)  # reference speed
         m.ref_cte = pyo.Param(initialize=0.0, mutable=True)  # reference lat error
         m.ref_epsi = pyo.Param(initialize=0.0, mutable=True)  # reference heading error
         m.states0 = pyo.Param(pyo.RangeSet(0, self.N_s - 1),
-                              initialize={0: 0., 1: 0., 2: 0., 3: 0., 4: 0., 5: 0., 6: 0., 7: 0.},
+                              initialize={0: 0., 1: 0., 2: 0., 3: 0., 4: 0., 5: 0., 6: 0., 7: 0., 8: 0.},
                               mutable=True)  # state initial
-        m.goal = pyo.Param(pyo.RangeSet(0, 4),
-                           initialize={0: goal[0], 1: goal[1], 2: goal[2], 3: goal[3], 4: goal[4]})  # goal
+        m.goal = pyo.Param(pyo.RangeSet(0, 5),
+                           initialize={0: goal[0], 1: goal[1], 2: goal[2], 3: goal[3], 4: goal[4], 5: goal[5]})  # goal
 
         m.ref_trajx = pyo.Param(m.sNum, rule=lambda m, k: ref_traj[0, k])
         m.ref_trajy = pyo.Param(m.sNum, rule=lambda m, k: ref_traj[1, k])
@@ -167,81 +166,82 @@ class MPC(object):
 
         # Variables for variation
         m.states = pyo.Var(pyo.RangeSet(0, self.N_s - 1), m.sNum)  # states as var NsxNs
-        m.ua = pyo.Var(m.uNum, bounds=(self.ua_bound[0], self.ua_bound[1]))  # control acc as var
+        m.uj = pyo.Var(m.uNum, bounds=(self.uj_bound[0], self.uj_bound[1]))  # control jerk as var
         m.uo = pyo.Var(m.uNum, bounds=(self.uo_bound[0], self.uo_bound[1]))  # control steering as var
-        m.dt = pyo.Var(m.sNum, bounds=(0.01, 0.1))
+        m.dt = pyo.Var(initialize=0.1, bounds=(0.01, 1))
         # m.ref_traj = pyo.Var(pyo.RangeSet(0, 1), m.sNum)
 
-        # states&  0: x, 1: y, 2: theta, 3: v, 4: psi, 5: ctex, 6: ctey, 7.epsi
-        # u&  0: a, 1: omega
+        # states&  0: x, 1: y, 2: theta, 3: v, 4: psi, 5:acc,  6:ctex, 7: ctey, 8.epsi
+        # u&  0: jerk, 1: omega
 
         # Constraints
         # states_0 = states0
         m.states0_update = pyo.Constraint(pyo.RangeSet(0, self.N_s - 1),
                                           rule=lambda m, i: m.states[i, 0] == m.states0[i])
         # goal = states end
-        m.goal_update = pyo.Constraint(pyo.RangeSet(0, 4), rule=lambda m, k: m.goal[k] == m.states[k, N_p - 1])
+        m.goal_update = pyo.Constraint(pyo.RangeSet(0, 5), rule=lambda m, k: m.goal[k] == m.states[k, N_p - 1])
 
         ############### discretized Nonlinear Differential equations as constraints ######################
         # x' = x + v * cos(theta) * dt
         m.x_update = pyo.Constraint(m.sNum, rule=lambda m, k:
-        m.states[0, k + 1] == m.states[0, k] + m.states[3, k] * pyo.cos(m.states[2, k]) * m.dt[k]
+        m.states[0, k + 1] == m.states[0, k] + m.states[3, k] * pyo.cos(m.states[2, k]) * m.dt
         if k < N_p - 1 else pyo.Constraint.Skip)
 
         # y' = y + v * sin(theta) * dt
         m.y_update = pyo.Constraint(m.sNum, rule=lambda m, k:
         m.states[1, k + 1] == m.states[1, k] + m.states[3, k] * pyo.sin(
-            m.states[2, k]) * m.dt[k] if k < N_p - 1 else pyo.Constraint.Skip)
+            m.states[2, k]) * m.dt if k < N_p - 1 else pyo.Constraint.Skip)
 
         # th' = th + v * tan(psi) / Lf * dt
         m.theta_update = pyo.Constraint(m.sNum, rule=lambda m, k:
         m.states[2, k + 1] == m.states[2, k] + m.states[3, k] * pyo.tan(
-            m.states[4, k]) / m.Lf * m.dt[k] if k < N_p - 1 else pyo.Constraint.Skip)
+            m.states[4, k]) / m.Lf * m.dt if k < N_p - 1 else pyo.Constraint.Skip)
 
         # v' = v + a * dt
         m.v_update = pyo.Constraint(m.sNum, rule=lambda m, k:
-        m.states[3, k + 1] == m.states[3, k] + m.ua[k] * m.dt[k] if k < N_p - 1 else pyo.Constraint.Skip)
+        m.states[3, k + 1] == m.states[3, k] + m.states[5, k] * m.dt if k < N_p - 1 else pyo.Constraint.Skip)
 
         # psi' = psi + omega * dt
         m.omega_update = pyo.Constraint(m.sNum, rule=lambda m, k:
-        m.states[4, k + 1] == m.states[4, k] + m.uo[k] * m.dt[k] if k < N_p - 1 else pyo.Constraint.Skip)
+        m.states[4, k + 1] == m.states[4, k] + m.uo[k] * m.dt if k < N_p - 1 else pyo.Constraint.Skip)
+
+        # a' = a + jerk * dt
+        m.a_update = pyo.Constraint(m.sNum, rule=lambda m, k:
+        m.states[5, k + 1] == m.states[5, k] + m.uj[k] * m.dt if k < N_p - 1 else pyo.Constraint.Skip)
 
         # ctex = x_ref - x - v * cos(theta) * dt
         m.ctex_update = pyo.Constraint(m.sNum, rule=lambda m, k:
-        m.states[5, k + 1] == (m.ref_trajx[k] - m.states[0, k] - m.states[3, k] * pyo.cos(
-            m.states[2, k]) * m.dt[k]) if k < N_p - 1 else pyo.Constraint.Skip)
+        m.states[6, k + 1] == (m.ref_trajx[k] - m.states[0, k] - m.states[3, k] * pyo.cos(
+            m.states[2, k]) * m.dt) if k < N_p - 1 else pyo.Constraint.Skip)
 
         # ctey = y_ref - y - v * sin(theta) * dt
         m.ctey_update = pyo.Constraint(m.sNum, rule=lambda m, k:
-        m.states[6, k + 1] == (m.ref_trajy[k] - m.states[1, k] - m.states[3, k] * pyo.sin(
-            m.states[2, k]) * m.dt[k]) if k < N_p - 1 else pyo.Constraint.Skip)
+        m.states[7, k + 1] == (m.ref_trajy[k] - m.states[1, k] - m.states[3, k] * pyo.sin(
+            m.states[2, k]) * m.dt) if k < N_p - 1 else pyo.Constraint.Skip)
 
         # epsi = heading - th - v * tan(psi) / Lf * dt
         m.epsi_update = pyo.Constraint(m.sNum, rule=lambda m, k:
-        m.states[7, k + 1] == (m.ref_heading[k] - m.states[2, k] - m.states[3, k] * pyo.tan(
-            m.states[4, k]) / m.Lf * m.dt[k]) if k < N_p - 1 else pyo.Constraint.Skip)
+        m.states[8, k + 1] == (m.ref_heading[k] - m.states[2, k] - m.states[3, k] * pyo.tan(
+            m.states[4, k]) / m.Lf * m.dt) if k < N_p - 1 else pyo.Constraint.Skip)
 
         # Objective function
-        m.vobj = m.wg[0] * sum((m.states[3, k] - m.ref_v) ** 2 for k in m.sNum)  # wg * ||v - v_ref||^2
+        m.vobj = m.wg[2] * sum((m.states[3, k] - m.ref_v) ** 2 for k in m.sNum)  # wg * ||v - v_ref||^2
         m.uoobj = m.wg[1] * sum(m.uo[k] ** 2 for k in m.uNum)  # wg * ||uo||^2
-        m.uaobj = m.wg[1] * sum(m.ua[k] ** 2 for k in m.uNum)  # wg * ||ua||^2
+        m.ujobj = m.wg[1] * sum(m.uj[k] ** 2 for k in m.uNum)  # wg * ||uj||^2
 
         m.suoobj = m.wg[3] * sum((m.uo[k + 1] - m.uo[k]) ** 2 for k in m.uNum1)  # sum: wg * ||uo_next - uo||^2
-        m.suaobj = m.wg[3] * sum((m.ua[k + 1] - m.ua[k]) ** 2 for k in m.uNum1)  # sum: wg * ||a_next - a||^2
+        m.sujobj = m.wg[3] * sum((m.uj[k + 1] - m.uj[k]) ** 2 for k in m.uNum1)  # sum: wg * ||j_next - j||^2
 
-        m.ctexobj = m.wg[2] * sum((m.states[5, k] - m.ref_cte) ** 2 for k in m.sNum)  # sum: wg * ||ctex - cte_ref||^2
-        m.cteyobj = m.wg[2] * sum((m.states[6, k] - m.ref_cte) ** 2 for k in m.sNum)  # sum: wg * ||ctey - cte_ref||^2
-        m.epsiobj = m.wg[2] * sum((m.states[7, k] - m.ref_epsi) ** 2 for k in m.sNum)  # sum: wg * ||epsi - epsi_ref||^2
+        m.ctexobj = m.wg[2] * sum((m.states[6, k] - m.ref_cte) ** 2 for k in m.sNum)  # sum: wg * ||ctex - cte_ref||^2
+        m.cteyobj = m.wg[2] * sum((m.states[7, k] - m.ref_cte) ** 2 for k in m.sNum)  # sum: wg * ||ctey - cte_ref||^2
+        m.epsiobj = m.wg[2] * sum((m.states[8, k] - m.ref_epsi) ** 2 for k in m.sNum)  # sum: wg * ||epsi - epsi_ref||^2
 
-        m.timeobj = m.wg[5] * sum(m.dt[k] ** 2 for k in m.sNum)
+        m.timeobj = m.wg[5] * sum(m.dt ** 2 for k in m.sNum)
         m.distanceobj = m.wg[5] * sum(
             (m.states[0, k + 1] - m.states[0, k]) ** 2 + (m.states[1, k + 1] - m.states[1, k]) ** 2 for k in m.uNum1)
 
-        # m.goalobj = m.wg[4] * sum(
-        #     (m.goal[k] - m.states[k, N_p - 1]) ** 2 for k in range(5))  # wg * ||traj_end - goal||^2
-
         m.obj = pyo.Objective(
-            expr=m.ctexobj + m.cteyobj + m.epsiobj + m.vobj + m.uoobj + m.uaobj + m.suoobj + m.suaobj + m.timeobj + m.distanceobj,
+            expr=m.ctexobj + m.cteyobj + m.epsiobj + m.vobj + m.uoobj + m.ujobj + m.suoobj + m.timeobj + m.distanceobj,
             sense=pyo.minimize)
 
         return m  # .create_instance()
@@ -260,20 +260,20 @@ class MPC(object):
         else:
             self.model.states0.reconstruct(
                 {0: state[0], 1: state[1], 2: state[2], 3: state[3], 4: state[4], 5: state[5], 6: state[6],
-                 7: state[7]})
+                 7: state[7], 8: state[8]})
 
         pyo.SolverFactory('ipopt').solve(self.model)
 
         x_pred_vals = [self.model.states[0, k]() for k in self.model.sNum]
         y_pred_vals = [self.model.states[1, k]() for k in self.model.sNum]
-
-        v_forward = [self.model.states[3, k]() for k in self.model.uNum]
-        steering_angle = [self.model.states[4, k]() for k in self.model.uNum]
-        acc = [self.model.ua[k]() for k in self.model.uNum]
+        th_pred_vals = [self.model.states[2, k]() for k in self.model.sNum]
+        v_forward = [self.model.states[3, k]() for k in self.model.sNum]
+        steering_angle = [self.model.states[4, k]() for k in self.model.sNum]
+        acc = [self.model.states[5, k]() for k in self.model.sNum]
         omega = [self.model.uo[k]() for k in self.model.uNum]
-        dt = [self.model.dt[k]() for k in self.model.sNum]
+        dt = self.model.dt()
 
-        return [x_pred_vals, y_pred_vals, acc, omega, dt], np.array([v_forward, steering_angle])
+        return [x_pred_vals, y_pred_vals, th_pred_vals, acc, omega, dt], np.array([v_forward, steering_angle])
 
 
 if __name__ == '__main__':
---
+++ MPC_with_RS/cal_angle_from_track.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def mod_theta(theta, t_last):
+    if abs(theta - t_last) > np.pi / 2:
+        if np.sign(theta) > 0:
+            return theta - np.pi
+        else:
+            return theta + np.pi
+
+    return (theta + np.pi) % (2 * np.pi) - np.pi
+
+
+def generate_orientation(track, starter):
+    l_track = len(track.T)
+    vlast = starter[:2]
+    vec_list = []
+    th_list = []
+    vec_list.append(starter)
+    th_list.append(starter[2])
+    t_last = starter[2]
+    for i in range(1, l_track):
+        vec = get_vector(track[:, i - 1], track[:, i], "+")
+        if check_flip(vlast, vec):
+            sign = "-"
+            # print("flipped!")
+        else:
+            sign = "+"
+
+        vec_ = get_vector(np.array([0, 0]), vec, sign)
+        theta = np.arctan2(vec_[1], vec_[0])
+        theta_mod = mod_theta(theta, t_last)
+        th_list.append(theta_mod)
+        vec_list.append(np.hstack((vec_, [theta])))
+        vlast = vec_
+        t_last = theta_mod
+
+    return np.array(vec_list).T, np.array(th_list)
+
+
+def check_flip(vlast, v_cur):
+    ip = vlast[0] * v_cur[0] + vlast[1] * v_cur[1]
+    if ip < 0:
+        return True
+    else:
+        return False
+
+
+def get_vector(p1, p2, sign):
+    if sign == "+":
+        return p2 - p1
+    else:
+        return p1 - p2
+
+
+test_track = np.array([[0, 1, 1.5, 1., 0.],
+                       [0, 1, 1.1, 1.9, 1.5]])
+diff_test_track = np.diff(test_track, axis=1)
+
+test_traj = np.load("saved_traj.npz", allow_pickle=True)
+trackx = test_traj["traj"][0]
+tracky = test_traj["traj"][1]
+trackth = np.array(test_traj["traj"][2])
+start = np.array([trackx[0], tracky[1], trackth[2]])
+track = np.vstack((trackx, tracky))
+
+vec_cal, theta_cal = generate_orientation(track, start)
+
+fig = plt.figure()
+ax1 = plt.subplot(211)
+ax1.plot(theta_cal * 180 / np.pi, label="theta_cal")
+ax1.plot(trackth * 180 / np.pi, label="ref_theta")
+ax1.grid(True)
+ax1.legend()
+
+ax1 = plt.subplot(212)
+ax1.plot((theta_cal - trackth) * 180 / np.pi, label="theta_error")
+ax1.legend()
+ax1.grid(True)
+plt.show()
+print(1)
